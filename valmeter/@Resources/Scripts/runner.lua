405db7b96a0b8b68c16aac193a30b0f39e8f2980
frissyn <62220201+frissyn@users.noreply.github.com>
2021-03-28 20:35:36 -0400

    ✨: update runner script

--- valmeter/@Resources/Scripts/runner.lua
+++ valmeter/@Resources/Scripts/runner.lua
@@ -1,3 +1,21 @@
+discordRPC = require("discordRPC")
+
+
+function discordRPC.ready(userId, username, discriminator, avatar)
+    print(string.format("RPC Ready: [%s](%s,#%s)", userId, username, discriminator))
+end
+
+
+function discordRPC.disconnected(errorCode, message)
+    print(string.format("RPC Disconnect: [%d: %s]", errorCode, message))
+end
+
+
+function discordRPC.errored(errorCode, message)
+    print(string.format("RPC Error: (%d: %s)", errorCode, message))
+end
+
+
 function Update()
     local gameProc = SKIN:GetMeasure('GameProcess'):GetValue()
     local clientProc = SKIN:GetMeasure('ClientProcess'):GetValue()
